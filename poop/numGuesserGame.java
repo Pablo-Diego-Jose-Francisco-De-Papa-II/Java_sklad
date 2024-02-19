987f696ef649639d094d098bde7f655bed179518
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-02-19 17:34:58 +0100

    Update numGuesserGame.java

--- poop/numGuesserGame.java
+++ poop/numGuesserGame.java
@@ -1,9 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 import java.lang.Math;
 
 /* todo list
-* add user difficulty 
+* add user difficulty
 * add helper ✅
 * maybe keep score
 * progressively increase dif
@@ -33,38 +34,54 @@ public class GuessGame {
                 case "classic" -> {
                     minNum = 1;
                     maxNum = 10;
-                    randomNumber = rand.nextInt(maxNum - minNum + 1) + minNum;
+                    randomNumber = rand.nextInt(maxNum) + minNum;
+
                     shouldRun = false;
                 }
 
                 case "normal" -> {
                     System.out.print("\nWith how many digits you wanna play: ");
-                    int digits = input.nextInt();
-                    minNum = 1;
-                    maxNum = (int) Math.pow(10, digits) - 1;
-                    randomNumber = rand.nextInt(maxNum - minNum + 1) + minNum;
-                    shouldRun = false;
+
+                    try {
+                        int digits = input.nextInt();
+                        minNum = 1;
+                        maxNum = (int) Math.pow(10, digits) - 1;
+                        randomNumber = rand.nextInt(maxNum) + minNum;
+
+                        shouldRun = false;
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Next time enter a number.");
+                    }
+
                 }
 
                 case "custom" -> {
                     System.out.print("\nSet a range \"num - num\": ");
-                    String nums = input.nextLine();
-                    String[] idk = nums.trim().split("-");
 
-                    minNum = Integer.parseInt(idk[0].trim());
-                    maxNum = Integer.parseInt(idk[1].trim());
-                    randomNumber = rand.nextInt(maxNum - minNum + 1) + minNum;
+                    try {
+                        String nums = input.nextLine();
+                        String[] idk = nums.trim().split("-");
 
-                    shouldRun = false;
+                        minNum = Integer.parseInt(idk[0].trim());
+                        maxNum = Integer.parseInt(idk[1].trim());
+                        randomNumber = rand.nextInt(maxNum - minNum + 1) + minNum;
+
+                        shouldRun = false;
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input. Next time enter a number.");
+                    }
+
                 }
 
-                default -> System.out.println("Learn how to write");
+                default -> System.out.print("\nWhat mode you want to play: ");
             }
         }
 
 
         String randomNumberStr = Integer.toString(randomNumber);
-        System.out.println("How to play: 1.Guess number between " + minNum + " - " + maxNum);
+        System.out.println("Your goal is to guess number between " + minNum + " - " + maxNum + "!");
 
 
         while (true) {
