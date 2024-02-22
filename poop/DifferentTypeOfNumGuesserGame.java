cb53fbce56b618d61fd90c3aa22c387850685999
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-02-22 09:48:53 +0100

    Revert "Update DifferentTypeOfNumGuesserGame.java"

--- poop/DifferentTypeOfNumGuesserGame.java
+++ poop/DifferentTypeOfNumGuesserGame.java
@@ -26,10 +26,6 @@ public class UhrinovejHra {
                 splitedNumber.add(Integer.valueOf(item));
             }
 
-            if (String.valueOf(randomNumber).trim().equals(number.trim())) {
-                return;
-            }
-
             for (int i = 0; i < splitedRandomNumber.size(); i++) {
 
                 if (splitedNumber.get(i).equals(splitedRandomNumber.get(i))) {
