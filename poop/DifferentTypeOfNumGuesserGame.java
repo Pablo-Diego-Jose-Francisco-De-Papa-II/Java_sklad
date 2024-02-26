d2fece5f4da426b958ce16416a576ebb537028cc
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-02-26 21:29:41 +0100

    FINISHED

--- poop/DifferentTypeOfNumGuesserGame.java
+++ poop/DifferentTypeOfNumGuesserGame.java
@@ -8,19 +8,24 @@ import java.util.Arrays;
 public class UhrinovejHra {
     static Random rand = new Random();
     static Scanner input = new Scanner(System.in);
-    static StringBuilder str = new StringBuilder();
+    static StringBuilder pandigitalNumber = new StringBuilder();
 
     public static void main(String[] args) {
         int[] allNumbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int attempts = 0;
+        int randomNumber = 0;
+        boolean shouldRun = true;
 
         System.out.println("Chose your mode (random \"r\" or pandigital \"p\"):");
-        String modeChooser= input.next();
 
-        switch (modeChooser) {
-            case "random", "r" -> { int randomNumber = randomRandomNumber(100000, 999999); }
-            case "pandigital", "p" -> { int randomNumber = randomPandigitalNumber(allNumbers); }
-            default -> System.out.println("idk");
+        while (shouldRun) {
+            String modeChooser= input.next();
+
+            switch (modeChooser) {
+                case "random", "r" -> { randomNumber = randomRandomNumber(100000, 999999); shouldRun = false; }
+                case "pandigital", "p" -> { randomNumber = randomPandigitalNumber(allNumbers); shouldRun = false; System.out.println(randomNumber); }
+                default -> System.out.println("This mode doesnt exist...");
+            }
         }
 
         while (true) {
@@ -64,13 +69,14 @@ public class UhrinovejHra {
         return rand.nextInt(maxNum - minNum + 1) + minNum;
     }
 
-    public static int randomPandigitalNumber(Integer[] allNumbers) {
-        Collections.shuffle(Arrays.asList(allNumbers));
+    public static int randomPandigitalNumber(int[] allNumbers) {
+        Integer[] allNumbersInteger = Arrays.stream(allNumbers).boxed().toArray(Integer[]::new);
+        Collections.shuffle(Arrays.asList(allNumbersInteger));
 
         for (int i = 0; i < 6; i++) {
-            str.append(allNumbers[i]);
+            pandigitalNumber.append(allNumbersInteger[i]);
         }
-        return Integer.parseInt(str.toString());
+        return Integer.parseInt(pandigitalNumber.toString());
     }
 
     public static ArrayList<Integer> listIt(String str) {
