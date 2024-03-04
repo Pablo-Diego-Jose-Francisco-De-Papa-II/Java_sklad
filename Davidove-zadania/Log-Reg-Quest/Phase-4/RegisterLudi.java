7d31689e35d31d068fb8a40ecc774a4a1b3c24ad
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-03-04 21:37:52 +0100

    Update RegisterLudi.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4/RegisterLudi.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4/RegisterLudi.java
@@ -1,3 +1,15 @@
-public class RegisterLudi {
+import java.util.ArrayList;
 
+public class RegisterLudi {
+    static ArrayList<Clovek> peopleDetails = new ArrayList<>();
+
+    public static void registerPerson(Clovek clovek) {
+        peopleDetails.add(clovek);
+    }
+
+    public void printAllPeople() {
+        for (Clovek person : peopleDetails) {
+            System.out.print(person + " ");
+        }
+    }
 }
