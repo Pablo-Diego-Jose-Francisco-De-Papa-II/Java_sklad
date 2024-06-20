96c84c74eb5fffd394728d49d77bcc80f8395c58
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-20 21:42:15 +0200

    Update RegisterLudi.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4/RegisterLudi.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4/RegisterLudi.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class RegisterLudi {
-    static ArrayList<Clovek> peopleDetails = new ArrayList<>();
+    static ArrayList<Clovek> register = new ArrayList<>();
 
-    public static void registerPerson(Clovek clovek) {
-        peopleDetails.add(clovek);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {t
+            System.out.println("Enter your name: ");
+            String name = input.nextLine();
+
+            System.out.println("Enter your age: ");
+            int age = Integer.parseInt(input.nextLine());
+
+            register.add(new Clovek(name, age));
+
+            System.out.println("Wanna see all registered users (yes/no) or end?: ");
+            switch (input.nextLine().toLowerCase()) {
+                case "yes", "y" -> printRegister();
+                case "end", "e" -> { return; }
+            }
+        }
     }
 
-    public void printAllPeople() {
-        for (Clovek person : peopleDetails) {
-            System.out.print(person + " ");
+    static void printRegister() {
+        for (Clovek atri : register) {
+            System.out.println(atri.name + " " + atri.age);
         }
     }
 }
