180531054a19cac37d0291ed5a64b6eb238e76ab
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-23 12:22:52 +0200

    Update and rename idk to Rarity.java

---
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
@@ -0,0 +1,13 @@
+package DavitZadania.Zadanie4_1;
+
+public class Rarity {
+
+     public enum rarity {
+        COMMON,
+        UNCOMMON,
+        RARE,
+        EPIC,
+        LEGENDARY,
+        MYTHIC
+    }
+}
