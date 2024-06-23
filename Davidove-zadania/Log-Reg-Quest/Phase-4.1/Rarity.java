d5359af9c0e8b9179cafa3028c33c2b4520f37a0
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-23 23:05:08 +0200

    Update Rarity.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
@@ -1,11 +1,27 @@
 package DavitZadania.Zadanie4_1;
 
 
- public enum Rarity {
-    COMMON,
-    UNCOMMON,
-    RARE,
-    EPIC,
-    LEGENDARY,
-    MYTHIC
+import java.util.Random;
+
+public enum Rarity {
+    COMMON(1, new String[]{"Pallerovej čajík", "Ing. titul"}),
+    UNCOMMON(1.3, new String[]{"Brianrot", "RTX 4090"}),
+    RARE(1.9, new String[]{"Femboy", "Filament"}),
+    EPIC(2.5, new String[]{"Hyperion", "Davitov freetime"}),
+    LEGENDARY(3.3, new String[]{"Cum sock", "Nezvladam to"}),
+    MYTHIC(generateMultiplication(), new String[]{"Priepustka z chemickej", "Sranie allowance", "Mayove vayo"});
+
+
+    double multiplication;
+    String[] itemName;
+    Rarity(double value, String[] array) {
+        this.multiplication = value;
+        this.itemName = array;
+    }
+
+
+    static int generateMultiplication() {
+        Random random = new Random();
+        return random.nextInt(500, 1001);
+    }
 }
