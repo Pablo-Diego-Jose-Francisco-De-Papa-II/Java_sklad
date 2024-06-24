7ca1ddb2a1076b5d0f6f726a746cf362a874ecee
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-24 20:40:39 +0200

    Update Gamba.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
@@ -1,25 +1,18 @@
 package DavitZadania.Zadanie4_1;
 
 import java.util.Scanner;
-import java.util.Random;
 
-import static DavitZadania.Zadanie4_1.Rarity.item;
-import static DavitZadania.Zadanie4_1.Rarity.itemPicker;
+import static DavitZadania.Zadanie4_1.Rarity.*;
 
 
 public class Gamba {
-    static Random random = new Random();
-    static int randomNum;
-    static Rarity rarity;
-    static int price;
-
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         while (true) {
             switch (input.nextLine()) {
                 case "inv" -> System.out.print("shows inv frfr");
+                case "end" -> { return; }
                 case "" -> gamble();
             }
         }
@@ -27,38 +20,8 @@ public class Gamba {
 
 
     static void gamble() {
-        randomNum = random.nextInt(100) + 1;
-
         rarityPricePicker();
         itemPicker();
         System.out.println("Unlocked " + rarity + " rarity item (" + item + ") and It's worth " + price + " coins!");
-    }
-
-
-    static void rarityPricePicker() {
-        if (randomNum < 35) {
-            rarity = Rarity.COMMON;
-            price = randomNum;
-        }
-        else if (randomNum < 60) {
-            rarity = Rarity.UNCOMMON;
-            price = (int)Math.round(randomNum * Rarity.UNCOMMON.multiplication);
-        }
-        else if (randomNum < 85) {
-            rarity = Rarity.RARE;
-            price = (int)Math.round(randomNum * Rarity.RARE.multiplication);
-        }
-        else if (randomNum < 95) {
-            rarity = Rarity.EPIC;
-            price = (int)Math.round(randomNum * Rarity.EPIC.multiplication);
-        }
-        else if (randomNum < 100) {
-            rarity = Rarity.LEGENDARY;
-            price = (int)Math.round(randomNum * Rarity.LEGENDARY.multiplication);
-        }
-        else if (randomNum == 100) {
-            rarity = Rarity.MYTHIC;
-            price = (int)Rarity.MYTHIC.multiplication;
-        }
     }
 }
