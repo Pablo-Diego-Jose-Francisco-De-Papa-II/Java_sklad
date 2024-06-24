a6e66e3c5b9ce9154c012beaa78a98a41e7f8af6
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-24 20:40:21 +0200

    Update Rarity.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
@@ -4,17 +4,19 @@ package DavitZadania.Zadanie4_1;
 import java.util.Random;
 
 public enum Rarity {
-    COMMON(1),
-    UNCOMMON(1.3),
-    RARE(1.9),
-    EPIC(2.5),
-    LEGENDARY(3.3),
-    MYTHIC(new Random().nextInt(500, 1001));
+    COMMON(1, 34),
+    UNCOMMON(1.3, 59),
+    RARE(1.9, 84),
+    EPIC(2.5, 94),
+    LEGENDARY(3.3, 99),
+    MYTHIC(new Random().nextInt(500, 1001), 100);
 
 
-    final double multiplication;
-    Rarity(double value) {
-        this.multiplication = value;
+    final int chance;
+    final double value;
+    Rarity(double value, int chance) {
+        this.chance = chance;
+        this.value = value;
     }
 
 
@@ -24,5 +26,20 @@ public enum Rarity {
 
     static void itemPicker() {
         item = allItems[new Random().nextInt(allItems.length)];
+    }
+
+
+    static Rarity rarity;
+    static int price;
+    static void rarityPricePicker() {
+        final int randomNum = new Random().nextInt(100) + 1;
+
+        for (Rarity r : values()) {
+            if (randomNum <= r.chance) {
+                rarity = r;
+                price = (int)(randomNum * r.value);
+                break;
+            }
+        }
     }
 }
