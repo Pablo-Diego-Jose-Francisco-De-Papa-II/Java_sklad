20114cb8c3ba2fceb237efc83388c372453077b6
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-25 00:34:34 +0200

    Update Rarity.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Rarity.java
@@ -1,45 +1,44 @@
-package DavitZadania.Zadanie4_1;
+    package DavitZadania.Zadanie4_1;
 
 
-import java.util.Random;
+    import java.util.Random;
 
-public enum Rarity {
-    COMMON(1, 34),
-    UNCOMMON(1.3, 59),
-    RARE(1.9, 84),
-    EPIC(2.5, 94),
-    LEGENDARY(3.3, 99),
-    MYTHIC(new Random().nextInt(500, 1001), 100);
-
-
-    final int chance;
-    final double value;
-    Rarity(double value, int chance) {
-        this.chance = chance;
-        this.value = value;
-    }
+    public enum Rarity {
+        COMMON(1, 34),
+        UNCOMMON(1.3, 59),
+        RARE(1.9, 84),
+        EPIC(2.5, 94),
+        LEGENDARY(3.3, 99),
+        MYTHIC(new Random().nextInt(500, 1001), 100);
 
 
-    static String item;
-    static final String[] allItems = {"Pallerovej moč", "Ing. titul", "Brianrot", "RTX 4090", "Femboy", "Filament",
-            "Hyperion", "Davitov freetime", "Priepustka z chemickej", "Sranie allowance", "Mayove vayo"};
+        final int chance;
+        final double value;
+        Rarity(double value, int chance) {
+            this.chance = chance;
+            this.value = value;
+        }
 
-    static void itemPicker() {
-        item = allItems[new Random().nextInt(allItems.length)];
-    }
+        static String item;
+        static final String[] allItems = {"Pallerovej moč", "Ing. titul", "Brianrot", "RTX 4090", "Femboy", "Filament",
+                "Hyperion", "Davitov freetime", "Priepustka z chemickej", "Sranie allowance", "Mayove vayo"};
+
+        static void itemPicker() {
+            item = allItems[new Random().nextInt(allItems.length)];
+        }
 
 
-    static Rarity rarity;
-    static int price;
-    static void rarityPricePicker() {
-        final int randomNum = new Random().nextInt(100) + 1;
+        static Rarity rarity;
+        static int price;
+        static void rarityPricePicker() {
+            final int randomNum = new Random().nextInt(100) + 1;
 
-        for (Rarity r : values()) {
-            if (randomNum <= r.chance) {
-                rarity = r;
-                price = (int)(randomNum * r.value);
-                break;
+            for (Rarity r : values()) {
+                if (randomNum <= r.chance) {
+                    rarity = r;
+                    price = (int)(randomNum * r.value);
+                    break;
+                }
             }
         }
     }
-}
