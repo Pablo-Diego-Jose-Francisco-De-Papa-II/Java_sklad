40d48fb63de2f58fa8c8d6dc65183ca400d121db
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-06-25 00:34:21 +0200

    Create OpenedItems.java

---
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/OpenedItems.java
@@ -0,0 +1,14 @@
+package DavitZadania.Zadanie4_1;
+
+
+public class OpenedItems {
+    String item;
+    String rarity;
+    int price;
+
+    public OpenedItems(String pickedItem, String pickedRarity, int pickedPrice) {
+        this.item = pickedItem;
+        this.rarity = pickedRarity;
+        this.price = pickedPrice;
+    }
+}
