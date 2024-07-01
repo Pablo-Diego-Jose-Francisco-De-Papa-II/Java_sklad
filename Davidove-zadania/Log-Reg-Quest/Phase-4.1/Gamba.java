7348fce712ff3c07e5fbb1fbc22de1e3b4b994ba
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-07-01 22:16:51 +0200

    Update Gamba.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
@@ -1,17 +1,30 @@
 package DavitZadania.Zadanie4_1;
 
-import static DavitZadania.Zadanie4_1.Items.*;
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Gamba {
+    static ArrayList<Item> itemList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         while (true) {
             switch (input.nextLine()) {
-                case "inv" -> System.out.print("shows inv frfr");
+                case "inv" -> {
+                    switch (input.nextLine()) {
+                        case "item", "i" -> System.out.println("idkItem");
+                        case "rarity", "r" -> System.out.println("idk1");
+                        case "price", "p" -> {
+                            itemList.sort(new Sort());
+                            for (Item idk : itemList) {
+                                System.out.println(idk);
+                            }
+                        }
+                        case "time", "t" -> System.out.println("idk3");
+                    }
+                }
                 case "end" -> { return; }
                 case "" -> gamble();
             }
@@ -20,7 +33,9 @@ public class Gamba {
 
 
     static void gamble() {
-        Items Item = new Items(getRandomItem(), getRandomRarity(), getRandomPrice());
-        System.out.println("Unlocked " + Item.rarity + " rarity item (" + Item.item + ") and It's worth " + Item.price + " coins!");
+        Item item = new Item();
+
+        itemList.add(item);
+        System.out.println("Unlocked " + item.rarity + " rarity item (" + item.item + ") and It's worth " + item.price + " coins!");
     }
 }
