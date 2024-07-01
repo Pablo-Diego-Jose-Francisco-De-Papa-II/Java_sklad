78d7ca023a7e266b707550696dc0b354162782ad
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-07-01 22:16:15 +0200

    Update Item.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Item.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Item.java
@@ -1,5 +1,7 @@
 package DavitZadania.Zadanie4_1;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 import static DavitZadania.Zadanie4_1.Rarity.*;
@@ -9,18 +11,27 @@ public class Item {
     String item;
     Rarity rarity;
     int price;
+    String time;
 
-    
+
     static final Random rand = new Random();
     final int randomNum = rand.nextInt(100) + 1;
+    static final DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    
+
     public Item() {
         this.item = getRandomItem();
         this.rarity = getRandomRarity();
         this.price = getRandomPrice();
+        this.time = getTime();
     }
-    
+
+
+    @Override
+    public String toString() {
+        return rarity + " " + item + " " + price + " coins " + time;
+    }
+
 
     public String getRandomItem() {
         return Rarity.ALL_ITEMS[rand.nextInt(Rarity.ALL_ITEMS.length)];
@@ -36,7 +47,7 @@ public class Item {
         return COMMON;
     }
 
-    
+
     int getRandomPrice() {
         for (Rarity r : values()) {
             if (randomNum <= r.chance) {
@@ -44,5 +55,10 @@ public class Item {
             }
         }
         return 1;
+    }
+
+
+    public String getTime() {
+        return LocalTime.now().format(myFormatObj);
     }
 }
