3954c0f4591c5f21b63d9d670997d046228089c7
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-07-03 12:16:24 +0200

    Update Gamba.java

--- Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-4.1/Gamba.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class Gamba {
     static ArrayList<Item> itemList = new ArrayList<>();
+    static boolean printInReverse = false;
+
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -16,27 +18,19 @@ public class Gamba {
                     switch (input.nextLine()) {
                         case "item", "i" -> {
                             itemList.sort(Item.comparator4);
-                            for (Item idk : itemList) {
-                                System.out.println(idk);
-                            }
+                            choice();
                         }
                         case "rarity", "r" -> {
                             itemList.sort(Item.comparator2);
-                            for (Item idk : itemList) {
-                                System.out.println(idk);
-                            }
+                            choice();
                         }
                         case "price", "p" -> {
                             itemList.sort(Item.comparator1);
-                            for (Item idk : itemList) {
-                                System.out.println(idk);
-                            }
+                            choice();
                         }
                         case "time", "t" -> {
                             itemList.sort(Item.comparator3);
-                            for (Item idk : itemList) {
-                                System.out.println(idk);
-                            }
+                            choice();
                         }
                     }
                 }
@@ -52,5 +46,29 @@ public class Gamba {
 
         itemList.add(item);
         System.out.println("Unlocked " + item.rarity + " rarity item (" + item.item + ") and It's worth " + item.price + " coins!");
+    }
+
+
+    public static void choice() {
+        if (printInReverse) {
+            helpReversed();
+        } else {
+            helpNormal();
+        }
+    }
+
+    public static void helpNormal() {
+        for (Item idk : itemList) {
+            System.out.println(idk);
+        }
+        printInReverse = true;
+    }
+
+    public static void helpReversed() {
+        System.out.println("reversed");
+        for (Item idk : itemList.reversed()) {
+            System.out.println(idk);
+        }
+        printInReverse = false;
     }
 }
