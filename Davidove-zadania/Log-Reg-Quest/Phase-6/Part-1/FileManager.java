409f1069fe087c633f1cb1382af3088d8a1f1e1f
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-07-30 23:09:35 +0200

    Update FileManager.java

--- Davidove-zadania/Log-Reg-Quest/Phase-6/Part-1/FileManager.java
+++ Davidove-zadania/Log-Reg-Quest/Phase-6/Part-1/FileManager.java
@@ -8,18 +8,96 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FileManager {
-    public static void main(String[] args) throws IOException {
-        File txtPath = new File("C:\\Users\\echni\\IdeaProjects\\JavaTheHutt\\src\\DavitZadania\\Zadanie6\\text.txt");
-        FileWriter txtWriter = new FileWriter (txtPath);
-        FileReader txtReader = new FileReader (txtPath);
+    private static File file;
 
-        Scanner file = new Scanner(txtReader);
+    
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("What would you like to do: ");
 
-        txtWriter.write(input.nextLine());
-        txtWriter.close();
+            switch (input.nextLine()) {
+                case "open", "o" -> {
+                    System.out.println("Choose file to open: ");
+                    FileManager.openAndGetFile(input.nextLine());
+                }
+                case "create", "c" -> {
+                    System.out.println("What should the file be named: ");
+                    FileManager.createAndGetFile(input.nextLine());
+                }
+                case "write", "w" -> {
+                    FileManager.write(input.nextLine());
+                }
+                case "writeAll", "wa" -> {//todo
+                    return;
+                }
+                case "read", "r" -> {//todo
+                    return;
+                }
+                case "readAll", "ra" -> {//todo
+                    return;
+                }
+                case "end", "e" -> {
+                    return;
+                }
+            }
+        }
+    }
 
-        System.out.println(file.nextLine());
+    public static FileManager openAndGetFile(String fileName) {
+        try {
+            return new FileManager(new File(fileName).getAbsoluteFile());
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static FileManager createAndGetFile(String fileName) {
+        File newFile = null;
+
+        try {
+            newFile = new File(fileName);
+            newFile.createNewFile();
+
+            return new FileManager(file);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public FileManager(File file) {
+        this.file = file;
+    }
+
+    public static void write(String text) {
+        try {
+            FileWriter txtWriter = new FileWriter(file);
+
+            Scanner input = new Scanner(text);
+
+            txtWriter.write(input.nextLine());
+            txtWriter.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public String writeAll(String[] text) {
+        return "";
+    }
+
+    public void readAll() {
+
+    }
+
+    public void read(int i) {
+
     }
 }
+
