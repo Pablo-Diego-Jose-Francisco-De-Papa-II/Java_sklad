f59585fe1c44f22639defef24ab92525bb3c22ec
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2024-09-12 18:55:32 +0200

    Update and rename Phase-1.java to Zadanie1.java

--- davitove_zadania/Zadanie1.java
+++ davitove_zadania/Zadanie1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Davitov_vadny_login_register_pindik {
+public class Zadanie1 {
 
     public static void main(String[] args) {
 
